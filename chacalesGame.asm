d016a8324fb9bb2b57a0688ca4c2f1bd54039480
Moises Alvarez <mfalvare@espol.edu.ec>
2024-06-14 15:41:18 -0500

    Anadiendo tarea
    Ejecuta el archivo asm desde mars

--- chacalesGame.asm
+++ chacalesGame.asm
@@ -1,1 +1,13 @@
-# Chales Game
+.data
+    # Definir la cadena a imprimir
+    hello_msg: .asciiz "Hello, World!\n"
+
+.text
+    # Cargar el código para imprimir cadena
+    li $v0, 4          # Cargar el código 4 para imprimir cadena
+    la $a0, hello_msg  # Cargar la dirección de la cadena en $a0
+    syscall            # Llamar al sistema para imprimir la cadena
+
+    # Terminar el programa
+    li $v0, 10         # Cargar el código 10 para terminar el programa
+    syscall            # Llamar al sistema para salir
