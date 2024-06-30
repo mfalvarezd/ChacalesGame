fc4280c1686474f60b25215cc00ce2a5841296f7
Moises Alvarez <mfalvare@espol.edu.ec>
2024-06-30 01:39:53 -0500

    corrigiendo casillaYaEncontrada

--- chacalesGame.asm
+++ chacalesGame.asm
@@ -104,10 +104,7 @@ whileNoWin:
 
     li      $v0,                10                                                                                  # Llamar al sistema para salir
     syscall 
-casillaYaEncontrada:        
-    li      $v0,                4
-    la      $a0,                msgCasillaEncontrada
-    syscall 
+
 encontroChacal:             
     li      $v0,                4
     la      $a0,                chacal
@@ -123,16 +120,26 @@ validarHallazgo:
 
     jr      $ra                                                                                                     # Retornar de la subrutina
 actualizar_tablero:         
+    # Verificar si la casilla ya ha sido descubierta
     la      $t1,                descubiertas                                                                        # Cargar la dirección base del arreglo 'descubiertas'
     sll     $t0,                $t0,                        2                                                       # Multiplicar el índice por 4 (tamaño de palabra)
-    add     $t0,                $t0,                        $t1                                                     # Calcular la dirección del elemento del arreglo
+    add     $t0,                $t0,                        $t1                                                     # Calcular la dirección del elemento del arreglo 'descubiertas'
+    lw      $t2,                0($t0)                                                                              # Cargar el valor actual de la casilla descubiertas[$t0]
 
-    li      $t2,                1                                                                                   # Valor que queremos almacenar en el arreglo
-    sw      $t2,                0($t0)                                                                              # Almacenar el valor 1 en la posición calculada del arreglo
+    bnez    $t2,                casillaYaEncontrada                                                                 # Saltar a casillaYaEncontrada si ya está descubierta
 
+    # Si la casilla no ha sido descubierta, marcarla como descubierta
+    li      $t2,                1                                                                                   # Valor que queremos almacenar en el arreglo (casilla descubierta)
+    sw      $t2,                0($t0)                                                                              # Almacenar el valor 1 en la posición calculada del arreglo 'descubiertas'
 
     jr      $ra                                                                                                     # Retornar de la subrutina
 
+casillaYaEncontrada:        
+    li      $v0,                4
+    la      $a0,                msgCasillaEncontrada
+    syscall 
+
+    jr      $ra                                                                                                     # Retornar de la subrutina
 wait_for_enter:             
     la      $a0,                messageContinuar
     syscall 
